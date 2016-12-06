c042942a6cf384141132ccd9a8b10c0167fbd3db
l.shepherd <l.shepherd@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-12-06 13:23:06 +0000

    Fixed broken functionality and made updates
      - Added Imports to Description
      - listResources queries cache, not system directory
      - addResource returns unique rid 
      - addResource has save option 
      - addResource can save object or path location
      - new function loadResource
      - update removeResource to operate on list of rids
      - fixed broken removeCache
      - updtaed sql functions appropriately to above changes
      - sql add helper function to sql_get_entry 
      - Added sqlite table entry for path to file 
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocFileCache@124848 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/schema/BiocFileCache.sql
+++ inst/schema/BiocFileCache.sql
@@ -2,15 +2,16 @@
 CREATE TABLE resource (
     rid INTEGER PRIMARY KEY AUTOINCREMENT,
     rname text,
+    filepath text,
     create_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     cache_file_name text
 );
 -- INSERT
 INSERT INTO resource (
-    rname, cache_file_name
+    rname, filepath, cache_file_name
 ) VALUES (
-    '%s', '%s'
+    '%s', '%s', '%s'
 );
-SELECT last_insert_rowid()
+SELECT last_insert_rowid();
 -- REMOVE
-DELETE FROM resource WHERE rids IN (%s);
+DELETE FROM resource WHERE rid IN (%s);
