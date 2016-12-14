2f038f0e8f4d5e90208f58e3438c9e1c20d9863c
l.shepherd <l.shepherd@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-12-14 18:40:25 +0000

    Updates
      - Changed schema only one path 
      - Added [ and [[ subsetting
      - Added newResource for placeholder resource
      - Modified addResource for tracking files not objects
      - Modified loadResource to give path
      - Modified updateResource - update path or rname
      - Added helper sql functions and documentation to match 
      
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocFileCache@125158 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/schema/BiocFileCache.sql
+++ inst/schema/BiocFileCache.sql
@@ -4,14 +4,13 @@ CREATE TABLE resource (
     rname text,
     create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
     access_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-    cache_file_name text,
     cache_file_path text
 );
 -- INSERT
 INSERT INTO resource (
-    rname, cache_file_name, cache_file_path
+    rname, cache_file_path
 ) VALUES (
-    '%s', '%s', '%s'
+    '%s', '%s'
 );
 SELECT rid FROM resource WHERE ROWID = last_insert_rowid();
 -- REMOVE
@@ -23,4 +22,8 @@ WHERE rid = '%d';
 -- UPDATE_TIME
 UPDATE resource 
 SET access_time = CURRENT_TIMESTAMP
+WHERE rid = '%d';
+-- UPDATE_RNAME
+UPDATE resource 
+SET rname = '%s', access_time = CURRENT_TIMESTAMP
 WHERE rid = '%d';
