a60773ddb7d69577ade9a1d02abc70b452f8de5c
Martin Morgan <mtmorgan@fhcrc.org>
2017-02-27 02:54:29 +0000

    make rid character() rather than integer
    * version bump
    
    * update code, examples, vignette for character() rid
    - explicit index 5 -> "BFC5"
    - as.integer(names(...)) -> names(...)
    - [, [[ methods defined on character rather than numeric
    
    * remove unused .sql_full_resource_table()
    
    * drop 'id' when reading tbl
    
    * update schema so rid is TEXT
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocFileCache@126985 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/schema/BiocFileCache.sql
+++ inst/schema/BiocFileCache.sql
@@ -1,13 +1,14 @@
 -- TABLE
 CREATE TABLE resource (
-    rid INTEGER PRIMARY KEY AUTOINCREMENT,
-    rname text,
+    rid TEXT,
+    rname TEXT,
     create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
     access_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-    rpath text, 
-    rtype text, 
-    fpath text,
-    last_modified_time DATETIME DEFAULT CURRENT_TIMESTAMP
+    rpath TEXT,
+    rtype TEXT,
+    fpath TEXT,
+    last_modified_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    id INTEGER PRIMARY KEY AUTOINCREMENT
 );
 -- INSERT
 INSERT INTO resource (
@@ -15,30 +16,31 @@ INSERT INTO resource (
 ) VALUES (
     '%s', '%s', '%s', '%s'
 );
+UPDATE resource SET rid = "BFC" || id WHERE ROWID = last_insert_rowid();
 SELECT rid FROM resource WHERE ROWID = last_insert_rowid();
 -- REMOVE
 DELETE FROM resource WHERE rid IN (%s);
 -- UPDATE_PATH
-UPDATE resource 
+UPDATE resource
 SET rpath = '%s', access_time = CURRENT_TIMESTAMP
-WHERE rid = '%d';
+WHERE rid = '%s';
 -- UPDATE_TIME
-UPDATE resource 
+UPDATE resource
 SET access_time = CURRENT_TIMESTAMP
-WHERE rid = '%d';
+WHERE rid = '%s';
 -- UPDATE_RNAME
-UPDATE resource 
+UPDATE resource
 SET rname = '%s', access_time = CURRENT_TIMESTAMP
-WHERE rid = '%d';
+WHERE rid = '%s';
 -- UPDATE_MODIFIED
-UPDATE resource 
+UPDATE resource
 SET last_modified_time  = '%s', access_time = CURRENT_TIMESTAMP
-WHERE rid = '%d';
+WHERE rid = '%s';
 -- UPDATE_FPATH
-UPDATE resource 
+UPDATE resource
 SET fpath = '%s', access_time = CURRENT_TIMESTAMP
-WHERE rid = '%d';
+WHERE rid = '%s';
 -- QUERY_NAMES
-SELECT rid FROM resource 
-WHERE rname || rpath || fpath 
+SELECT rid FROM resource
+WHERE rname || rpath || fpath
 LIKE '%%%s%%';
