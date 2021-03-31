1c12b01868e120eca9f44b817eb3d54a292e4a26
Lori Shepherd <lori.shepherd@roswellpark.org>
2021-03-31 12:53:24 -0400

    Update caching location to use tools instead of rappdirs

---
+++ R/zzz.R
@@ -0,0 +1,27 @@
+.onLoad <- function(libname, pkgname) {
+
+    cacheChange <- "1.15.1"
+    pkgVersion <- as.character(packageVersion(pkgname))
+
+    if ( utils::compareVersion(cacheChange, pkgVersion) < 0){
+
+        require(rappdirs)
+
+        olddefault <- rappdirs::user_cache_dir(appname="BiocFileCache")
+
+        newlocation <- tools::R_user_dir("BiocFileCache", which="cache")
+
+        if (dir.exists(olddefault) && (length(list.files(olddefault)) != 0)){
+
+            msg <- sprintf(
+                "As of %s (> %s), The default caching location has changed.\n  To avoid redownloading previously cached files and use previouly existing default cache\n  see BiocFileCache vignette section:\n  'Default Caching Location Update'",
+                pkgname,
+                cacheChange)
+
+
+            packageStartupMessage(paste(strwrap(msg, exdent=2), collapse="\n"))
+
+        }
+
+    }
+}
